a8bee601325763d2c48815fd25744931d208f39b
Shishmakov Vladimir <vova2005761@gmail.com>
2025-11-27 00:42:15 +0300

    task 5 done

---
+++ main.py
@@ -0,0 +1,21 @@
+import math
+from itertools import count
+
+def rational_numbers():
+    yield (0, 1)
+
+    for s in count(1):
+        for q in range(1, s + 1):
+            p_abs = s - q
+            if p_abs == 0:
+                continue
+            if math.gcd(p_abs, q) != 1:
+                continue
+            yield (-p_abs, q)
+            yield (p_abs, q)
+
+
+gen = rational_numbers()
+for _ in range(50):
+    p, q = next(gen)
+    print(f"{p}/{q} = {p/q}")
